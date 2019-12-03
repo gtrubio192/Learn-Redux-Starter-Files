1426823c89094022cf4af27359e80fd523323132
Gabe Rubio <gtrubio192@gmail.com>
2019-12-03 14:32:47 -0600

    updating Comments component to get all props. updating comment reducer to post a comment

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -16,6 +16,31 @@
 
 function comments(state = [], action) {
   console.log(state, action);
+    if(typeof action.postId !=='undefined') {
+    return {
+      // take current state
+      ...state,
+      // overrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
+  
+  return state;
+}
+
+function postComments(state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT':
+      // RETURN NEW STATE WITH NEW COMMENT
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      return state;
+    default:
+      return state;
+  }
   return state;
 }
 
